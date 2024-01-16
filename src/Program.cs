c6004a41226b00362f5d18ae15beb1a063851e97
AnduinXue <anduin@aiursoft.com>
2024-01-16 12:30:47 +0000

    Move models.

--- src/Program.cs
+++ src/Program.cs
@@ -25,30 +25,6 @@ public static class ProgramExtends
         var services = scope.ServiceProvider;
         var logger = services.GetRequiredService<ILogger<Startup>>();
         var context = services.GetRequiredService<WingetServerDbContext>();
-        var mockData = new List<Package>
-        {
-            new()
-            {
-                Id = "Python.Python.3.11",
-                Name = "Python 3.11",
-                Version = "3.11.7",
-                Publisher = "Python Software Foundation"
-            },
-            new()
-            {
-                Id = "Python.Python.3.10",
-                Name = "Python 3.10",
-                Version = "3.10.11",
-                Publisher = "Python Software Foundation"
-            }
-        };
-        
-        var isNew = !await context.Packages.AnyAsync();
-        if (isNew)
-        {
-            logger.LogInformation("Seeding mock data...");
-            await context.Packages.AddRangeAsync(mockData);
-            await context.SaveChangesAsync();
-        }
+       
     }
 }
