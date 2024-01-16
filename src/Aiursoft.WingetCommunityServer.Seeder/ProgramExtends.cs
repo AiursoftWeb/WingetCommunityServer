94a785924255179c74697f7c0a88d84de0ad8e72
AnduinXue <anduin@aiursoft.com>
2024-01-16 13:15:53 +0000

    Really extract files.

--- src/Aiursoft.WingetCommunityServer.Seeder/ProgramExtends.cs
+++ src/Aiursoft.WingetCommunityServer.Seeder/ProgramExtends.cs
@@ -1,5 +1,7 @@
-﻿using Aiursoft.GitRunner;
+﻿using System.Text.RegularExpressions;
+using Aiursoft.GitRunner;
 using Aiursoft.GitRunner.Models;
+using Aiursoft.WingetCommunityServer.Database;
 using Aiursoft.WingetCommunityServer.Seeder.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -14,7 +16,7 @@ public static class ProgramExtends
     {
         using var scope = host.Services.CreateScope();
         var services = scope.ServiceProvider;
-        var logger = services.GetRequiredService<ILogger<object>>();
+        var logger = services.GetRequiredService<ILogger<WingetServerDbContext>>();
         //var context = services.GetRequiredService<WingetServerDbContext>();
         var storageConfig = services.GetRequiredService<IOptions<StorageConfig>>();
         var serverConfig = services.GetRequiredService<IOptions<ServerConfig>>();
@@ -26,6 +28,57 @@ public static class ProgramExtends
         logger.LogInformation("Pulling upstream...");
         await workspaceManager.ResetRepo(workPath, workBranch, cloneEndpoint,
             CloneMode.Depth1);
-        logger.LogInformation("Seeding database...");
+        logger.LogInformation("Extracing source...");
+        
+        var builtInPackages = new List<YamlPackage>();
+        
+        // find all .yaml files under workPath
+        var yamlFiles = Directory.GetFiles(workPath, "*.yaml", SearchOption.AllDirectories).Take(1000);
+        foreach (var yaml in yamlFiles)
+        {
+            var yamlContent = await File.ReadAllTextAsync(yaml);
+            var manifestType = Regex.Match(yamlContent, @"ManifestType: (.*)").Groups[1].Value;
+            if (manifestType.Trim() == "version")
+            {
+                var packageIdentifier = Regex.Match(yamlContent, @"PackageIdentifier: (.*)").Groups[1].Value;
+                var packageVersion = Regex.Match(yamlContent, @"PackageVersion: (.*)").Groups[1].Value;
+                var defaultLocale = Regex.Match(yamlContent, @"DefaultLocale: (.*)").Groups[1].Value;
+                var manifestVersion = Regex.Match(yamlContent, @"ManifestVersion: (.*)").Groups[1].Value;
+                var package = new YamlPackage
+                {
+                    PackageIdentifier = packageIdentifier,
+                    PackageVersion = packageVersion,
+                    DefaultLocale = defaultLocale,
+                    ManifestType = manifestType,
+                    ManifestVersion = manifestVersion
+                };
+                builtInPackages.Add(package);
+            }
+        }
+        
+        logger.LogInformation("Found {Count} packages.", builtInPackages.Count);
     }
 }
+
+public class YamlPackage
+{
+    public string PackageIdentifier { get; set; } = null!;
+    public string PackageVersion { get; set; } = null!;
+    public string DefaultLocale { get; set; } = null!;
+    public string ManifestType { get; set; } = null!;
+    public string ManifestVersion { get; set; } = null!;
+
+    public override string ToString()
+    {
+        return $"{PackageIdentifier} {PackageVersion}";
+    }
+}
+
+// # Created using wingetcreate 1.5.2.0
+// # yaml-language-server: $schema=https://aka.ms/winget-manifest.version.1.5.0.schema.json
+//
+// PackageIdentifier: 0x192.UniversalAndroidDebloaterGUI
+// PackageVersion: 0.5.1
+// DefaultLocale: en-US
+// ManifestType: version
+// ManifestVersion: 1.5.0
